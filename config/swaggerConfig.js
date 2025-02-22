3abc8b3568df8458a8c5b2d86f1a2f211fa86d99
Jeanphorka <jeanphorka@gmail.com>
2025-02-23 00:23:13 +0700

    setup

---
+++ config/swaggerConfig.js
@@ -0,0 +1,25 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const path = require('path');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'TSE-FONGDI Faculty Reporting API',
+            version: '1.0.0',
+            description: 'API documentation for the faculty reporting system'
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+                description: 'Local server'
+            }
+        ]
+    },
+    // ใช้ path.resolve() เพื่อชี้ไปที่ src/routes
+    apis: [path.resolve(__dirname, '../src/routes/*.js')]
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = specs;

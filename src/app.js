dc6812d7aaca503607a31d8f0fe2620db905e01d
Jeanphorka <jeanphorka@gmail.com>
2025-02-24 20:11:18 +0700

    เพิ่ม cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require("cors");
 const authRoutes = require('./routes/authRoutes');
 const dropdownRoutes = require('./routes/dropdownRoutes');
 const issueReportRoutes = require("./routes/issueReportRoutes");
@@ -7,20 +8,24 @@ require('dotenv').config();
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpecs = require('../config/swaggerConfig');
 
+
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // เรียก Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
-app.use("/api/dropdown", dropdownRoutes);
-app.use("/api/issue-reports", issueReportRoutes);
+
 // Routes
 app.use('/api/auth', authRoutes);
+app.use("/api/dropdown", dropdownRoutes);
+app.use("/api/issue-reports", issueReportRoutes);
 
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`🌐 Server running on port ${PORT}`);
     console.log(`📑 Swagger Docs: http://localhost:${PORT}/api-docs`);
